66fa52007bdf6ca1715b02ba66d034985d55488d
mamenyasu <laudo_shell@ybb.ne.jp>
2026-04-20 16:02:46 +0900

    データベースカラムにcommentを追加。ソースにも追加。

--- ReservationsModel.php
+++ ReservationsModel.php
@@ -26,18 +26,14 @@ class ReservationsModel{
 
     //予約追加メソッド。個人の予約データをreservationsテーブルにINSERTする。
     public function createReservation($request){
-        $room_id=$request['room_id'];
-        $user_name=$request['user_name'];
-        $user_telphone=$request['user_telphone'];
-        $checkin_date=$request['checkin_date'];
-        $checkout_date=$request['ceckout_date'];
         try{
-        $stmt=$this->pdo->prepare('INSERT INTO reservations (room_id,user_name,user_telphone,checkin_date,checkout_date) VALUES (:room_id,:user_name,:user_telphone,:checkin_date,:checkout_date)');
-        $stmt->bindValue(':room_id',$room_id,PDO::PARAM_INT);
-        $stmt->bindValue('user_name',$user_name,PDO::PARAM_STR);
-        $stmt->bindValue(':user_telphone',$user_telphone,PDO::PARAM_STR);
-        $stmt->bindValue(':checkin_date',$checkin_date,PDO::PARAM_STR);
-        $stmt->bindValue(':checkout_date',$checkout_date,PDO::PARAM_STR);
+        $stmt=$this->pdo->prepare('INSERT INTO reservations (room_id,user_name,user_telphone,comment,checkin_date,checkout_date) VALUES (:room_id,:user_name,:user_telphone,:comment,:checkin_date,:checkout_date)');
+        $stmt->bindValue(':room_id',$request['room_id'],PDO::PARAM_INT);
+        $stmt->bindValue('user_name',$request['user_name'],PDO::PARAM_STR);
+        $stmt->bindValue(':user_telphone',$request['user_telphone'],PDO::PARAM_STR);
+        $stmt->bindValue(':comment',$request['comment'],PDO::PARAM_STR);
+        $stmt->bindValue(':checkin_date',$request['checkin_date'],PDO::PARAM_STR);
+        $stmt->bindValue(':checkout_date',$request['checkout_date'],PDO::PARAM_STR);
         $stmt->execute();
         }catch(Exception $e){
             throw new Exception('データベースエラー：予約の登録に失敗しました');
@@ -58,10 +54,11 @@ class ReservationsModel{
     //予約変更（上書き）メソッド。
     public function updateReservation($request){
         try{
-        $stmt=$this->pdo->prepare('UPDATE reservations SET room_id=:room_id, user_name=:user_name, user_telphone=:user_telphone, checkin_date=:checkin_date, checkout_date=:checkout_date WHERE id=:id');
+        $stmt=$this->pdo->prepare('UPDATE reservations SET room_id=:room_id, user_name=:user_name, user_telphone=:user_telphone, comment=:comment, checkin_date=:checkin_date, checkout_date=:checkout_date WHERE id=:id');
         $stmt->bindValue(':room_id',$request['room_id'],PDO::PARAM_INT);
         $stmt->bindValue(':user_name',$request['user_name'],PDO::PARAM_STR);
         $stmt->bindValue(':user_telphone',$request['user_telphone'],PDO::PARAM_STR);
+        $stmt->bindValue(':comment',$request['comment'],PDO::PARAM_STR);
         $stmt->bindValue(':checkin_date',$request['checkin_date'],PDO::PARAM_STR);
         $stmt->bindValue(':checkout_date',$request['checkout_date'],PDO::PARAM_STR);
         $stmt->bindValue(':id',$request['id'],PDO::PARAM_INT);
@@ -71,24 +68,5 @@ class ReservationsModel{
         }
     }
 
-    //予約操作時開始時に、本当に空きがまだあるか確認するメソッド。trueかfalseを返す。
-    public function hasStock($request){
-        try{
-        $stmt=$this->pdo->prepare('SELECT booked_rooms,total_rooms FROM room_availability WHERE staydate >=:checkin_date AND staydate < :checkout_date');
-        $stmt->bindValue(':checkin_date',$request['checkin_date'],PDO::PARAM_STR);
-        $stmt->bindValue(':checkout_date',$request['checkout_date'],PDO::PARAM_STR);
-        $stmt->execute();
-
-        $found=false; //万が一、該当データが無く(10年先とか変なPOSTされた場合)、whileに入れなかった場合はfalseを返す為の変数。
-        while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
-            $found=true;
-            if($row['total_rooms']<=$row['booked_rooms']){
-                return false;
-            }
-        }
-        return $found;
-        }catch(Exception $e){
-            throw new Exception('データベースエラー：空き室状況の確認に失敗しました');
-        }
-    }
+    
 }
