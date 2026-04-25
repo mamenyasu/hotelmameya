53bf789b29ff588afeeb67f49ab24c408d3d6f55
mamenyasu <laudo_shell@ybb.ne.jp>
2026-04-25 23:55:39 +0900

    try cathの書き忘れ

--- services/PricesCalendarService.php
+++ services/PricesCalendarService.php
@@ -18,18 +18,22 @@ class PricesCalendarService{
     }
 //指定された部屋、指定された月という条件で、プランごとの料金表を返す。結果($prices)はプランごとの多重配列。
     public function getPricesAllPlan($room_id,$year,$month){
-        $prices=[];
+        try{
+            $prices=[];
 
-        //プランを配列で取得。
-        $plans=$this->planModel->getPlan();
+            //プランを配列で取得。
+            $plans=$this->planModel->getPlan();
 
-        foreach($plans as $plan){
+            foreach($plans as $plan){
             $pricesRecord=$this->pricesCalendarModel->getPricesRecord($room_id,$plan,$year,$month);
             $pricesArray=$this->roomMonthPriceService->getRoomMonthPrice($pricesRecord);  // [1=>3000,2=>3500 ...]みたいな感じに変換されて返ってくる。
             $prices[$plan]=$pricesArray; // ['bed_only'=>[1=>3000, 2=>3500 ...], 'standard'=>[1=>4000, 2=>4500 ...] ... ]みたいな感じになる。
+            }
+
+            return $prices;
+
+        }catch(Exception $e){
+            throw $e;
         }
-
-        return $prices;
-
     }
 }
