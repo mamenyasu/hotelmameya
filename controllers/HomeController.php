af91c056a4f4f2188750e9dc5f4f53d351645b28
mamenyasu <laudo_shell@ybb.ne.jp>
2026-04-27 00:11:24 +0900

    ホームコントローラに、セッション変数をクリアするプロセスを仕込んだ。これで、途中でバナーから別のページに飛んで再度戻ってきても安全なはず。

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,21 +1,36 @@
 <?php
 class HomeController{
+
+private function clearReservationSessions(){
+        unset($_SESSION['reserve']);
+        unset($_SESSION['reserve_cancel']);
+        unset($_SESSION['reserve_update_old']);
+        unset($_SESSION['reserve_update_new']);
+        unset($_SESSION['reserve_update_calendar']);
+        }
+
+
+
     public function index(){
+          $this->clearReservationSessions();
         include __DIR__.'/../views/index.php';
         exit();
     }
 
     public function rooms(){
+        $this->clearReservationSessions();
         include __DIR__.'/../views/rooms.php';
         exit();
     }
 
     public function foods(){
+        $this->clearReservationSessions();
         include __DIR__.'/../views/foods.php';
         exit();
     }
 
     public function amenity(){
+        $this->clearReservationSessions();
         include __DIR__.'/../views/amenity.php';
         exit();
     }
