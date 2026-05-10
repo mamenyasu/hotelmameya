75c89d54b92df9909e40382be3c798af3b545b74
mamenyasu <laudo_shell@ybb.ne.jp>
2026-05-10 22:37:44 +0900

    initial-scale=1を追記

--- views/reserveUpdateVerifyForm.php
+++ views/reserveUpdateVerifyForm.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>予約変更 | ホテルまめや</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="stylesheet" href="/hotelmameya/assets/css/style.css">
 </head>
 
